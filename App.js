01f302dc2d771af0835c6842818b867461dcce27
SANJAIDEVARAJ <67061462+SANJAIDEVARAJ@users.noreply.github.com>
2020-09-04 17:35:12 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
-import TodoList from "./components/todo-list.component";
+import CreateTodo from "./create-todo.component";
+import EditTodo from "./edit-todo.component";
+import TodoList from "./todo-list.component";
 
 class App extends Component {
   render() {
